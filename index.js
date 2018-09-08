7cd2e7e2cb512c0e3ba04bb4afbfb691a15c35a5
coord.e <me@coord-e.com>
2018-09-08 23:49:04 +0900

    Add: Use body-parser to extract message info from request

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 app.post('/', (req, res) => {
-  res.send('POST request to the homepage');
+  for (const event of req.body['events']) {
+    console.log(event.source['userId'])
+    console.log(event.message['text'])
+  }
+  res.status(200).end()
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
