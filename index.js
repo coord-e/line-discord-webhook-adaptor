8f39f35dee2e2773bc7ddf8525319fbca265003a
coord.e <me@coord-e.com>
2018-09-09 00:46:05 +0900

    Change: Support supplying port number from `PORT`

--- index.js
+++ index.js
@@ -45,4 +45,5 @@ app.post('/', asyncHandler(async (req, res) => {
   res.status(200).end()
 }))
 
-app.listen(3000, () => console.log('Listening on port 3000!'))
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}!`))
